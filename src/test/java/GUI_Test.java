2688ef5bb8af08a4cf90f734fb7d948a22f70371
bornhaeup <patrick.air@freenet.de>
2016-10-08 21:05:48 +0200

    d

--- src/test/java/GUI_Test.java
+++ src/test/java/GUI_Test.java
@@ -1,13 +1,14 @@
 package test.java;
-
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import main.java.GUI;
 
-public class Test {
+public class GUI_Test {
 
-	@org.junit.Test
-	public void test() {
+	@Test
+	public void testAdd() {
 		GUI gui = new GUI();
 		double result = gui.addition(1, 6);
 		assertEquals(7,result, 0);
